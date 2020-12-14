4a0146a6a57dce75b76fd03e5e62adae911f69ff
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2020-12-14 18:42:32 +0000

    first draft of the function

---
+++ message.py
@@ -0,0 +1,3 @@
+DROP_BTN = "Drop your local libs"
+EXPLAINATION = "By clicking on the `Drop` button, you will remove from your sepal docker the locally installed python libs. This operation will let you access all the sepal installed python libs which cover the mains geospatial and data analysis tools"
+DROPPED = "Your local libs have been droped successfully"
